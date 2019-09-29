dc194803d8cd991adec29ee265cc008a0548e081
Douglas Sondheim <douglassondheim@gmail.com>
2019-09-29 20:14:53 +0200

    add source manager

---
+++ sources/source_manager.go
@@ -0,0 +1,17 @@
+package sources
+
+import (
+	"github.com/DizzyDoug/extra-extra/sources/types"
+)
+
+type SourceManager struct {
+	sourceList []types.Source
+}
+
+func (sm SourceManager) AddSource(s types.Source) error {
+
+}
+
+func (sm SourceManager) RemoveSource(name string) error {
+
+}
